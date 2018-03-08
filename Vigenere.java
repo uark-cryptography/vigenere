617ed4643ad9445517254de032e1574f7c212d10
savaughn <savaughn@uark.edu>
2018-03-08 16:17:09 -0600

    kasiski differences

--- Vigenere.java
+++ Vigenere.java
@@ -6,9 +6,18 @@ class Vigenere{
 	
 	ArrayList<String> trigrams = new ArrayList<>();
 	ArrayList<String> trigramMatch = new ArrayList<>();
+	ArrayList<Integer> trigramPosition = new ArrayList<>();
 	
 	void print(String s){
 		System.out.println(s);
+	}
+	
+	void kasiski(ArrayList<String> trigrams, ArrayList<Integer> trigramPosition){
+		ArrayList<Integer> difference = new ArrayList<>();
+		for(int i = 0; i < trigramPosition.size()-1; i+=2){
+			difference.add(trigramPosition.get(i+1)-trigramPosition.get(i));
+		}
+		System.out.println("difference: " + difference);
 	}
 	
 	void findMatch(ArrayList<String> trigrams){
@@ -16,19 +25,21 @@ class Vigenere{
 			for(int j = i+1; j < trigrams.size(); j++){
 				if(trigrams.get(i).equals( trigrams.get(j))){
 					trigramMatch.add(trigrams.get(i));
+					trigramPosition.add(i+1);
+					trigramPosition.add(j+1);
 				}
 			}
 			
 		}
 		System.out.println("matches: " + trigramMatch);
+		System.out.println("position: " + trigramPosition);
+		kasiski(trigramMatch, trigramPosition);
 	}
 	
 	void trigram(String cT){
 		String t = "";
-		for(int i = 0; i < cT.length()-2; i++){
-					
-			for(int j = i; j < i+3; j++){
-				
+		for(int i = 0; i < cT.length()-2; i++){					
+			for(int j = i; j < i+3; j++){				
 				t += cT.charAt(j);
 			}
 			trigrams.add(t);
